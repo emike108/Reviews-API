b2a0a5d7470c3bf84ef31dab2fababad359a9a6a
Mike Ortiz <emike108@aim.com>
2021-11-24 23:28:42 -0800

    postReview method implemented

--- controllers/reviews.js
+++ controllers/reviews.js
@@ -88,7 +88,7 @@ const apiMethods = {
         const charValue = charValuesData[y].value
         generatedResponse.characteristics[charDescription].value += (charValue/numberOfReviews)
       }
-
+      console.log(`Successfully obtained metadata for product ${productId}`)
       res.status(200).json(generatedResponse)
 
     } catch(err) {
@@ -96,7 +96,55 @@ const apiMethods = {
       res.status(500).send(err)
     }
   },
-  postReviews: {},
+  postReviews: async (req, res) => {
+    try {
+      const productId = req.body.product_id
+      const rating = req.body.rating
+      const summary = req.body.summary
+      const body = req.body.body
+      const recommend = req.body.recommend
+      const reported = 'false'
+      const reviewerName = req.body.name
+      const reviewerEmail = req.body.email
+      const helpfulness = 0
+
+      const photos = req.body.photos
+      const characteristics = req.body.characteristics
+
+      const queryString1 = `INSERT INTO reviews (product_id, rating, date, summary, body, recommend, reported, reviewer_name, reviewer_email, response, helpfulness) VALUES (?, ?, UNIX_TIMESTAMP(), ?, ?, ?, ?, ?, ?, null, ?)`
+      const queryVariables = [productId, rating, summary, body, recommend, reported, reviewerName, reviewerEmail, helpfulness]
+      const reviewInsert = await db.query(queryString1, queryVariables)
+
+      const reviewEntryId = reviewInsert[0].insertId
+
+      if (photos.length > 0) {
+        for (let i = 0; i < photos.length; i++) {
+          const currentPhoto = photos[i]
+          const queryString2 = 'INSERT INTO reviews_photos (review_id, url) VALUES (?, ?)'
+          const photoInsert = await db.query(queryString2, [reviewEntryId, currentPhoto])
+        }
+      }
+      const charKeys = Object.keys(characteristics)
+
+      if (charKeys.length !== 0) {
+        const charValues = Object.values(characteristics)
+        for (let x = 0; x < charKeys.length; x++) {
+          const currentCharKey = charKeys[x]
+          const currentCharValue = charValues[x]
+          const queryString3 = 'INSERT INTO characteristics (product_id, name) VALUES (?, ?)'
+          const charEntry = await db.query(queryString3, [productId, currentCharKey])
+          const charEntryId = charEntry[0].insertId
+          const queryString4 = 'INSERT INTO characteristic_reviews (characteristic_id, review_id, value) VALUES (?, ?, ?)'
+          const charValueEntry = await db.query(queryString4, [charEntryId, reviewEntryId, currentCharValue])
+        }
+      }
+      console.log(`request added with review id ${reviewEntryId}`, req.body)
+      res.status(201).send(`Successfully added review ${reviewEntryId}!`)
+    } catch(err) {
+      console.log('Unable to add review.. ', err)
+      res.status(400).send(err)
+    }
+  },
   putHelpfulReview: {},
   putReportReview: {}
 }
--- routes/reviews.js
+++ routes/reviews.js
@@ -3,7 +3,7 @@ import apiMethods from '../controllers/reviews.js';
 const router = express.Router();
 
 router.get('/:product_id', apiMethods.getReviews);
-// router.post('/:product_id', apiMethods.postReviews);
+router.post('/:product_id', apiMethods.postReviews);
 router.get('/meta/:product_id', apiMethods.getMetadata);
 // router.put('/:review_id/helpful', apiMethods.putHelpfulReview);
 // router.put('/:review_id/report', apiMethods.putReportReview);
